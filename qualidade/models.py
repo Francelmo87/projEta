7a7bff9a8e75b5258d4c188723953c9cdc2aaf15
Francelmo Sousa da Silva <francelmo87@hotmail.com>
2022-05-26 23:30:42 -0300

    Criado o model parametros

--- qualidade/models.py
+++ qualidade/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Parameter(models.Model):
+    parameter = models.CharField(max_length=12, unique=True)
+
+    class Meta:
+        ordering = ('parameter',)
+
+    def __str__(self):
+        return self.parameter
