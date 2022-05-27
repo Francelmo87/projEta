f7917a8a5a005dc3142456c15a6f575a8e1d905c
Francelmo Sousa da Silva <francelmo87@hotmail.com>
2022-05-26 23:21:59 -0300

    criado o banco de dados de datas

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(
+        'Criado em',
+        auto_now_add=True,
+        auto_now=False
+    )
+    modified = models.DateTimeField(
+        'Modificado em',
+        auto_now_add=False,
+        auto_now=True
+    )
+
+    class Meta:
+        abstract = True
+
+
